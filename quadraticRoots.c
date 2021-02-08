7c8b7db8dda4b85d0bcd2fe3bb7b0ca1c2f85a3d
Aayushi82 <78746087+Aayushi82@users.noreply.github.com>
2021-02-08 23:48:31 +0530

    Create quadraticRoots.c

---
+++ quadraticRoots.c
@@ -0,0 +1,37 @@
+//to find roots of quadratic equation
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int rootsQuadratic(int ,int,int);
+void main()//main function
+{
+    int a,b,c;
+    printf("\n enter the values of a,b and c");
+    scanf("%d %d %d",&a,&b,&c);
+    rootsQuadratic(a,b,c);
+}
+int rootsQuadratic(int a,int b,int c)//function to find roots
+{
+   float d,deno,r1,r2;
+   d=(b*b)-(4*a*c);
+   deno=2*a;
+   if(d>0)
+   {
+       printf("\n real roots");
+       r1=(-b+sqrt(d))/deno;
+       r2=(-b+sqrt(d))/deno;
+       printf("\n root 1=%f \t root 2=%f",r1,r2);
+
+   }
+   else if (d==0)
+   {
+       printf("\n equal roots");
+       r1=-b/deno;
+       printf("root1=root2=%f",r1);
+
+   }
+   else
+   {
+       printf("imaginary roots");
+   }
+}
